137baa8731fe13a64636bdd6734762212724b244
cgm97 <cgm97@naver.com>
2024-12-23 23:46:50 +0900

    포트번호 properties 추가

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,6 +3,7 @@ require('dotenv').config(); // .env 파일에서 환경 변수 로드
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
