737f7c190c2d6febea0df0ecb66920ade71d396e
cgm97 <cgm97@naver.com>
2024-12-25 00:32:31 +0900

    </React.StrictMode>가 request 두번 호출하여 제거

--- client/src/index.js
+++ client/src/index.js
@@ -6,11 +6,16 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // React.StrictMode 제거
+  <App />
 );
 
+// React.StrictMode 제거 _ 2024 12 25 두번 request 호출하여 제거
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
