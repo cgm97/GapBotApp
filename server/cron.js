14319a0459caf0f9b635e4b5214ef6d273528ed5
cgm97 <cgm97@naver.com>
2024-12-26 22:36:35 +0900

    오류./..

--- server/cron.js
+++ server/cron.js
@@ -140,7 +140,7 @@ cron.schedule('* * * * *', async () => { // 1분마다 실행
         });
 
         logger.info({
-            method: '데이터확인1',
+            method: '데이터확인',
             url: '[CRON]',  // 요청 URL
             //message: `${JSON.stringify(arr, null, 2)} 모험섬 데이터 가공 종료`,
             message: `${arr}`
@@ -148,12 +148,6 @@ cron.schedule('* * * * *', async () => { // 1분마다 실행
 
         // 쿼리 실행 (다중 insert Promise.all - 병렬처리)
         const promises = arr.map(island =>
-            logger.info({
-                method: '데이터확인2',
-                url: '[CRON]',  // 요청 URL
-                //message: `${JSON.stringify(arr, null, 2)} 모험섬 데이터 가공 종료`,
-                message: `${island}`
-            });
             connection.execute(insertSql, [
                 island.BASE_DATE,
                 island.TIME_TYPE,
