a23a5a0763f01df329a0003728e265b611d02fd0
cgm97 <cgm97@naver.com>
2024-12-27 00:16:02 +0900

    리액트문법 class -> className 으로 변경

--- client/src/components/Top.js
+++ client/src/components/Top.js
@@ -3,11 +3,11 @@ import '../App.css'; // CSS 파일 (위에서 작성한 스타일을 참조)
 
 const Top = () => {
   return (
-    <div class="top">
-      <div class="container">
+    <div className="top">
+      <div className="container">
         <span><a href="https://open.kakao.com/o/g6Abem1g">빈틈봇커뮤니티</a></span>
       </div>
-      <div class="container">
+      <div className="container">
         <span><a href="#">로그인</a></span>
       </div>
     </div>
