802f3bccefa062b79ef286c250465a51db8f46e1
cgm97 <cgm97@naver.com>
2025-05-28 22:53:43 +0900

    meta img url 빈틈이미지로 변경

--- next-app/app/patchnote/page.js
+++ next-app/app/patchnote/page.js
@@ -9,7 +9,7 @@ export const metadata = {
     type: 'website',
     images: [
         {
-          url: '/img/logo.png',
+          url: 'https://loagap.com/img/logo.png',
           alt: `빈틈 이미지`
         }
       ]
