67921614013d2009def66481f9fdd1db08dea809
cgm97 <cgm97@naver.com>
2025-05-28 14:11:07 +0900

    패치노트 사이트 추가

---
+++ next-app/app/patchnote/page.js
@@ -0,0 +1,23 @@
+export const metadata = {
+  title: '패치노트 | LOAGAP',
+  description: '빈틈봇 패치노트 목록',
+  keywords: 'LOAGAP, 빈틈봇, 후원, 로스트아크, 로아봇, 군장봇, 군검봇, 유각, 보석, 겁화, 멸화, 작열 ,홍염 유물, 유각시세, 시세, 차트, 유각 차트',
+  openGraph: {
+    title: '패치노트 | LOAGAP',
+    description: '빈틈봇 패치노트 목록',
+    url: 'https://loagap.com/patchnote',
+    type: 'website',
+    images: [
+        {
+          url: '/img/logo.png',
+          alt: `빈틈 이미지`
+        }
+      ]
+  },
+};
+
+import PatchNotePage from './patchnoteClient';
+
+export default function Page() {
+  return <PatchNotePage />;
+}
