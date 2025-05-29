51012cfd831bce224259ad60879f61f835c1b0ef
KyuMin <65147348+cgm97@users.noreply.github.com>
2025-05-29 14:53:09 +0900

    캐릭터 조회 오류시 페이지가 먹통되는 현상 수정

--- next-app/app/character/[nickName]/page.js
+++ next-app/app/character/[nickName]/page.js
@@ -1,38 +1,49 @@
 import CharacterPage from './characterClient';
 
 export async function generateMetadata({ params }) {
-  const { nickName } = await params;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/character/search?nickName=${(nickName)}`, 
-  { 
-    headers: {
-      referer: 'loagap.com/meta'
-    },
-    next: { 
-      revalidate: 60 
-    } 
-  });
- 
-  const data = await res.json();
-  return {
-    title: `${data.profile.NICKNAME} 캐릭터 정보 | LOAGAP`,
-    description: `${data.profile.NICKNAME}님의 장비, 보석, 초월, 엘릭서 정보를 확인하세요.`,
-    openGraph: {
-      title: `${data.profile.TITLE !== "없음" ? data.profile.TITLE : ""} ${data.profile.NICKNAME} 캐릭터 정보 | LOAGAP`,
-      description: `${data.profile.SERVER} / ${data.guild.NAME} / Lv.${data.profile.ITEM_LEVEL} / ${data.profile.JOB}·${data.profile.SUBJOB}`,
-      url: `https://loagap.com/character/${data.profile.NICKNAME}`,
-      images: [
-        {
-          url: data.profile.IMG_URL,
-          width: 300,
-          height: 200,
-          alt: `${data.profile.NICKNAME}의 이미지`
-        }
-      ]
+  const { nickName } = params;
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/character/search?nickName=${encodeURIComponent(nickName)}`,
+      {
+        headers: {
+          referer: 'loagap.com/meta',
+        },
+        next: {
+          revalidate: 60,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      return {
+        title: '캐릭터 정보를 불러올 수 없습니다 | LOAGAP',
+        description: '잘못된 캐릭터 정보입니다.',
+      };
     }
-  };
-}
+
+    const data = await res.json();
+
+    return {
+      title: `${data.profile.NICKNAME} 캐릭터 정보 | LOAGAP`,
+      description: `${data.profile.NICKNAME}님의 장비, 보석, 초월, 엘릭서 정보를 확인하세요.`,
+      openGraph: {
+        title: `${data.profile.TITLE !== "없음" ? data.profile.TITLE : ""} ${data.profile.NICKNAME} 캐릭터 정보 | LOAGAP`,
+        description: `${data.profile.SERVER} / ${data.guild.NAME} / Lv.${data.profile.ITEM_LEVEL} / ${data.profile.JOB}·${data.profile.SUBJOB}`,
+        url: `https://loagap.com/character/${data.profile.NICKNAME}`,
+        images: [
+          {
+            url: data.profile.IMG_URL,
+            width: 300,
+            height: 200,
+            alt: `${data.profile.NICKNAME}의 이미지`
+          }
+        ]
+      }
+    };
+  }
 
 export default async function Page({ params }) {
-  const { nickName } = await params;
-  return <CharacterPage nickName={nickName} />;
-}
+    const { nickName } = await params;
+    return <CharacterPage nickName={nickName} />;
+  }
