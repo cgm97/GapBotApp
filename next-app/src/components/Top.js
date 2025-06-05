ecf86191ee15c50e0490b745096dc11cb83b4c4b
cgm97 <cgm97@naver.com>
2025-06-05 11:19:49 +0900

    헤더 및 Top 배열 모바일 고려 수정

--- next-app/src/components/Top.js
+++ next-app/src/components/Top.js
@@ -17,6 +17,7 @@ const Top = () => {
   };
 
   const searchCharacter = (inputValue) => {
+    setSearchInput("");
     router.push(`/character/${inputValue}`);
   };
 
@@ -28,36 +29,42 @@ const Top = () => {
 
   return (
     <div className="top">
-      <div className="container">
-        <a href="https://open.kakao.com/o/g6Abem1g" target="_blank" rel="noopener noreferrer">빈틈봇 소통방(분양)</a>&nbsp;&nbsp;
-        <Link href="/donate">후원안내</Link>
-      </div>
+  <div className="top-left">
+    <a href="https://open.kakao.com/o/g6Abem1g" target="_blank" rel="noopener noreferrer">
+      빈틈봇 소통방(분양)
+    </a>
+    &nbsp;&nbsp;
+    <Link href="/donate">후원안내</Link>
+  </div>
 
-      <div className="search-container">
-        <input
-          type="text"
-          className="search-box"
-          placeholder="캐릭터를 입력하세요..."
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        <button className="search-icon" onClick={() => searchCharacter(searchInput)}>
-          <i className="fa fa-search"></i>
-        </button>
-      </div>
-
-      <div className="container">
-        {user ? (
-          <>
-            <Link href="/mypage">{user}님 </Link>
-            <a href="#" onClick={handleLogout}>로그아웃</a>
-          </>
-        ) : (
-          <Link href="/login">로그인</Link>
-        )}
-      </div>
+  <div className="top-center">
+    <div className="search-container">
+      <input
+        type="text"
+        className="search-box"
+        placeholder="캐릭터를 입력하세요..."
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button className="search-icon" onClick={() => searchCharacter(searchInput)}>
+        <i className="fa fa-search"></i>
+      </button>
     </div>
+  </div>
+
+  <div className="top-right">
+    {user ? (
+      <>
+        <Link href="/mypage">{user}님 </Link>
+        <a href="#" onClick={handleLogout}>로그아웃</a>
+      </>
+    ) : (
+      <Link href="/login">로그인</Link>
+    )}
+  </div>
+</div>
+
   );
 };
 
